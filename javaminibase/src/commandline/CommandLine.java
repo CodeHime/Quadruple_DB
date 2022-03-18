9ab06b874b621793ed5c379850e9da735b966b94
Pavan Chikkathimmegowda <pavan.95.c@gmail.com>
2022-03-18 16:46:41 -0700

    fix line parsing

--- javaminibase/src/commandline/CommandLine.java
+++ javaminibase/src/commandline/CommandLine.java
@@ -1,6 +1,5 @@
 package commandline;
 
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -29,8 +28,8 @@ public class CommandLine {
 		// rdfDB database = rdfDB.getInstance();
 		// database.openrdfDB(dbname, type);
 
-
-		// Stream stream = database.openStream(Integer.parseInt(options[2]), options[3], options[4], options[5], Double.parseDouble(options[6]));
+		// Stream stream = database.openStream(Integer.parseInt(options[2]), options[3],
+		// options[4], options[5], Double.parseDouble(options[6]));
 
 		// for (QID qid = stream.getNext(); qid != null; qid = stream.getNext()) {
 
@@ -38,29 +37,36 @@ public class CommandLine {
 
 	}
 
-	public static void batchinsert(String options[]) throws IOException, InvalidPageNumberException, FileIOException, DiskMgrException {
+	//:Jorunn_Danielsen :knows :Eirik_Newth		0.5232176791516268
+	private static String[] processLine(String line) {
+		line = line.replaceAll(" ", "");
+		line = line.replaceFirst("\t", "");
+		line = line.replaceFirst("\t", ":");
+		line = line.replaceFirst(":", "");
+		return line.split(":");
+	}
+
+	public static void batchinsert(String options[])
+			throws IOException, InvalidPageNumberException, FileIOException, DiskMgrException {
 		System.out.println("batch");
 
 		try {
 			File f = new File(options[0]);
 			String dbname = options[2] + "_" + options[1];
-			SystemDefs sysdef = new SystemDefs(dbname, 1000 + 20, 1000, "Clock" );
+			SystemDefs sysdef = new SystemDefs(dbname, 1000 + 20, 1000, "Clock");
 			rdfDB database = rdfDB.getInstance();
 			System.out.println("rdfdb instance got");
 			int type = Integer.parseInt(options[1]);
 			database.openrdfDB(dbname, type);
 			System.out.println("rdfdb opened");
 
-
-
 			Scanner scanner = new Scanner(f);
 			while (scanner.hasNextLine()) {
 				String line = scanner.nextLine();
 
-				// String parts[] = line.replace('\t', ':').replaceAll(" ", "").split(":");
+				String parts[] = processLine(line);
 
-				// :Jorunn_Danielsen :knows :Eirik_Newth		0.5232176791516268
-				String parts[] = {"Jorunn", "knows", "Eirik_Newth", "0.5232176791516268"};
+
 				byte quad[] = new byte[32];
 
 				EID subjectid = database.insertEntity(parts[0]);
@@ -86,61 +92,55 @@ public class CommandLine {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
-
 	}
 
 	public static void getInput() throws InvalidPageNumberException, FileIOException, DiskMgrException {
-  
-  	int end = 0;
-  	while(end == 0){
-  
-  	BufferedReader in = new BufferedReader (new InputStreamReader(System.in));
-	
-	try {
-	   FileWriter fw;
-	   String input =  "batchinsert phase2_test_data.txt 1 testDB"; //in.readLine();
-	   String parsed[] = input.split(" ");
-	   PCounter.initialize();
-	   
-	   if (parsed[0].equals("report")&&parsed.length == 1){
-		   File f = new File("../logfile.txt");
-	   	Scanner scan = new Scanner(f);
-		   while(scan.hasNextLine()){
-			   System.out.println(scan.nextLine());
-		   }
-		   scan.close();
-	   }
-	   else if (parsed[0].equals("query")&&parsed.length == 9){
-	   	query(Arrays.copyOfRange(parsed,1,parsed.length));
-		//    fw = new FileWriter(parsed[1]+"_"+parsed[2]);
-		fw = new FileWriter("../logfile.txt");
-		   fw.write(input+"\n");
-		   fw.write("Reads "+PCounter.rcounter+"\nWrites: "+PCounter.wcounter+"\n\n");
-		   fw.close();
-		   System.out.println("Reads "+PCounter.rcounter+"\nWrites: "+PCounter.wcounter+"\n\n");
-	   }
-	   else if (parsed[0].equals("batchinsert")&&parsed.length == 4){
-	   	batchinsert(Arrays.copyOfRange(parsed,1,parsed.length));
-		//    fw = new FileWriter(parsed[3]+"_"+parsed[2]);
-		fw = new FileWriter("../logfile.txt");
-		   fw.write(input+"\n");
-		   fw.write("Reads "+PCounter.rcounter+"\nWrites: "+PCounter.wcounter+"\n\n");
-		   fw.close();
-		   System.out.println("Reads "+PCounter.rcounter+"\nWrites: "+PCounter.wcounter+"\n\n");
-	   }
-	   else if (parsed[0].equals("exit")||parsed[0].equals("quit")||parsed[0].equals("q")){
-		end = 1;	
-	   }
-	   else{
-	   	System.out.println("Unrecgonized command. Leave with 'exit'.");
-	   }
-	   
+
+		int end = 0;
+		while (end == 0) {
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+			try {
+				FileWriter fw;
+				String input = "batchinsert phase2_test_data.txt 1 testDB"; // in.readLine();
+				String parsed[] = input.split(" ");
+				PCounter.initialize();
+
+				if (parsed[0].equals("report") && parsed.length == 1) {
+					File f = new File("../logfile.txt");
+					Scanner scan = new Scanner(f);
+					while (scan.hasNextLine()) {
+						System.out.println(scan.nextLine());
+					}
+					scan.close();
+				} else if (parsed[0].equals("query") && parsed.length == 9) {
+					query(Arrays.copyOfRange(parsed, 1, parsed.length));
+					// fw = new FileWriter(parsed[1]+"_"+parsed[2]);
+					fw = new FileWriter("../logfile.txt");
+					fw.write(input + "\n");
+					fw.write("Reads " + PCounter.rcounter + "\nWrites: " + PCounter.wcounter + "\n\n");
+					fw.close();
+					System.out.println("Reads " + PCounter.rcounter + "\nWrites: " + PCounter.wcounter + "\n\n");
+				} else if (parsed[0].equals("batchinsert") && parsed.length == 4) {
+					batchinsert(Arrays.copyOfRange(parsed, 1, parsed.length));
+					// fw = new FileWriter(parsed[3]+"_"+parsed[2]);
+					fw = new FileWriter("../logfile.txt");
+					fw.write(input + "\n");
+					fw.write("Reads " + PCounter.rcounter + "\nWrites: " + PCounter.wcounter + "\n\n");
+					fw.close();
+					System.out.println("Reads " + PCounter.rcounter + "\nWrites: " + PCounter.wcounter + "\n\n");
+				} else if (parsed[0].equals("exit") || parsed[0].equals("quit") || parsed[0].equals("q")) {
+					end = 1;
+				} else {
+					System.out.println("Unrecgonized command. Leave with 'exit'.");
+				}
+
+			} catch (IOException e) {
+				System.out.println("error");
+			}
+		}
 	}
-	catch (IOException e) {
-		System.out.println("error");
-	}
-  	}
-  }
 
 	public static void main(String[] argvs) throws InvalidPageNumberException, FileIOException, DiskMgrException {
 		System.out.println("Hello, It's working");

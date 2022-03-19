04728027c968aa8943f4fb847f9f6dc4a5007bd9
Pavan Chikkathimmegowda <pavan.95.c@gmail.com>
2022-03-18 22:10:42 -0700

    resolve conflict

--- javaminibase/src/global/QuadrupleOrder.java
+++ javaminibase/src/global/QuadrupleOrder.java
@@ -1,41 +1,12 @@
 package global;
 
 /** 
-<<<<<<< HEAD
- * Enumeration class for TupleOrder
-=======
  * Enumeration class for QuadrupleOrder
->>>>>>> Branching out Stream and modifying as per logic
  * 
  */
 
 public class QuadrupleOrder {
 
-<<<<<<< HEAD
-  public static final int Ascending  = 0;
-  public static final int Descending = 1;
-  public static final int Random     = 2;
-
-  public int quadrupleOrder;
-
-  /** 
-   * TupleOrder Constructor
-   * <br>
-   * A tuple ordering can be defined as 
-   * <ul>
-   * <li>   TupleOrder quadrupleOrder = new TupleOrder(TupleOrder.Random);
-   * </ul>
-   * and subsequently used as
-   * <ul>
-   * <li>   if (quadrupleOrder.quadrupleOrder == TupleOrder.Random) ....
-   * </ul>
-   *
-   * @param quadrupleOrder The possible ordering of the tuples 
-   */
-
-  public QuadrupleOrder (int quadrupleOrder) {
-    quadrupleOrder = quadrupleOrder;
-=======
   public static final int Random     = 0;
   public static final int SubjectPredicateObjectConfidence = 1;
   public static final int PredicateSubjectObjectConfidence = 2;
@@ -61,25 +32,12 @@ public class QuadrupleOrder {
    * @param _quadupleOrder The possible ordering of the tuples 
    */
 
-  public TupleOrder (int _quadupleOrder) {
-    quadupleOrder = _quadupleOrder;
->>>>>>> Branching out Stream and modifying as per logic
+  public QuadrupleOrder (int _quadupleOrder) {
+    this._quadupleOrder = _quadupleOrder;
   }
 
   public String toString() {
-    
-<<<<<<< HEAD
-    switch (quadrupleOrder) {
-    case Ascending:
-      return "Ascending";
-    case Descending:
-      return "Descending";
-    case Random:
-      return "Random";
-    }
-    return ("Unexpected TupleOrder " + quadrupleOrder);
-=======
-    switch (tupleOrder) {
+    switch (_quadupleOrder) {
     case SubjectPredicateObjectConfidence:
       return "SubjectPredicateObjectConfidence";
     case PredicateSubjectObjectConfidence:
@@ -95,8 +53,7 @@ public class QuadrupleOrder {
     case Random:
       return "Random";
     }
-    return ("Unexpected TupleOrder " + tupleOrder);
->>>>>>> Branching out Stream and modifying as per logic
+    return ("Unexpected TupleOrder " + _quadupleOrder);
   }
 
 }

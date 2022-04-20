71c27a75a167a4638404a3ea85779ab7f1146eac
CodeHime <krima282@gmail.com>
2022-04-20 10:52:09 -0700

    Join in progress

--- javaminibase/src/basicpattern/BP_Triple_Join.java
+++ javaminibase/src/basicpattern/BP_Triple_Join.java
@@ -35,32 +35,29 @@ public class BP_Triple_Join implements GlobalConst {
 
 	/** The rdfDB we are using. */
 	private rdfDB _rdfdb;
-	int _orderType = 0;
-	boolean _needSort;
-	String _subjectFilter, _predicateFilter, _objectFilter;
 	double _minConfidence = 0.6;
 	String _minConfidenceFilter = Double.toString(_minConfidence);
 	Scan qfs;
 
-	boolean _subjectNullFilter = false;
-	boolean _predicateNullFilter = false;
-	boolean _objectNullFilter = false;
-	boolean _confidenceNullFilter = false;
-	boolean scan_on_btree = false;
-
 	EID _subjectID = new EID();
 	PID _predicateID = new PID();
 	EID _objectID = new EID();
-
-	//int SORT_Q_NUM_PAGES = SystemDefs.JavabaseBM.getNumBuffers()/2;
-	int SORT_Q_NUM_PAGES = 16;
 	
 	Sort qsort = null;
-	Heapfile _results=null;
-	QuadrupleHeapfile _rightQuads = null;
+	// Heapfile _results=null;
+	// QuadrupleHeapfile _rightQuads = null;
 	public TScan quadover = null;
 
-	boolean use_index = true;
+	BasicPatternIterator left_itr = null;
+	Stream rstream = null;
+
+	int amt_of_mem;
+	int num_left_nodes;
+	int BPJoinNodePosition;
+	int JoinOnSubjectorObject;
+	int [] LeftOutNodePositions;
+    int OutputRightSubject; 
+	int OutputRightObject;
 
 	/**
 	 * The constructor pins the first directory page in the file
@@ -83,31 +80,32 @@ public class BP_Triple_Join implements GlobalConst {
             // 
 
             try{
-                rdfDB database = rdfDB.getInstance();
-                Stream rstream = null;
-                Stream lstream = null;
-                QuadrupleHeapfile _leftQuads;
+                _rdfdb = rdfDB.getInstance();
+                // Stream lstream = null;
+                // QuadrupleHeapfile _leftQuads;
 
                 // Get all quads with minimum confidence
-                lstream = new Stream(database, "*","*", "*", _minConfidenceFilter);            
-                _leftQuads =  lstream.getResults();
-                
+                // lstream = new Stream(database, "*","*", "*", _minConfidenceFilter);            
+                // _leftQuads =  lstream.getResults();
+                this.left_itr=left_itr;
 				
                 // Get all filtered Quads for the join
-                rstream = new Stream(database, RightSubjectFilter, RightPredicateFilter, RightObjectFilter, Double.toString(RightConfidenceFilter));            
-                _rightQuads =  rstream.getResults();
+                rstream = new Stream(_rdfdb, RightSubjectFilter, RightPredicateFilter, RightObjectFilter, Double.toString(RightConfidenceFilter));            
+                
+				this.amt_of_mem=amt_of_mem;
+				this.num_left_nodes=num_left_nodes;
+				this.BPJoinNodePosition=BPJoinNodePosition;
+				this.JoinOnSubjectorObject=JoinOnSubjectorObject;
+				this.LeftOutNodePositions=LeftOutNodePositions;
+				this.OutputRightSubject=OutputRightSubject; 
+				this.OutputRightObject=OutputRightObject;
+	
+				// _rightQuads =  rstream.getResults();
 
+			
 				// JOIN
-                _results = new Heapfile(Long.toString((new java.util.Date()).getTime()));
-				
-				
-
+                // _results = new Heapfile(Long.toString((new java.util.Date()).getTime()));
 				// qfs = new Scan(_results);
-
-
-				lstream.closestream();
-                rstream.closestream();
-				// qfs.closescan();
             }
             catch(Exception e){
                 System.err.println(e);
@@ -123,11 +121,21 @@ public class BP_Triple_Join implements GlobalConst {
 		QID qid = new QID();
 
 
+
 	}
 
 	public void close() throws IOException, SortException{
-		// try{
-		// 	if()
-		// }
+		try{
+			if(rstream!=null){
+				// lstream.closestream();
+                rstream.closestream();
+				// qfs.closescan();
+			}
+		} catch (Exception e) {
+			System.out.println("Error in closing BT_Triple_Join");
+			e.printStackTrace();
+		}
+
+
 	}
 }

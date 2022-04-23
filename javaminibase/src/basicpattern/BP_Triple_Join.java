afbd4e260ae946657e4fa5ba3dcc1bc49dc3cf51
CodeHime <krima282@gmail.com>
2022-04-23 11:23:07 -0700

    add stream excepts to sort index join

--- javaminibase/src/basicpattern/BP_Triple_Join.java
+++ javaminibase/src/basicpattern/BP_Triple_Join.java
@@ -386,15 +386,40 @@ public class BP_Triple_Join implements GlobalConst {
 						e.printStackTrace();
 					}
 					if (JoinOnSubjectorObject == 0){
-						rstream = new Stream(_rdfdb, QuadrupleOrder.SubjectConfidence, 
-						outer_bp.getEIDFld(BPJoinNodePosition).getValue(), 
-						RightPredicateFilter, RightObjectFilter, RightConfidenceFilter);
+						try{
+							rstream = new Stream(_rdfdb, outer_bp.getEIDFld(BPJoinNodePosition).getValue(), 
+							RightPredicateFilter, RightObjectFilter, RightConfidenceFilter);
+						}
+						catch(Exception e){
+							try{
+								rstream.closestream();
+								rstream = null;
+								continue;
+							}
+							catch(Exception ex){
+								rstream = null;
+								continue;
+							}
+						}
 					}
 					else{
-						rstream = new Stream(_rdfdb, QuadrupleOrder.ObjectConfidence, 
-						RightSubjectFilter, RightPredicateFilter, 
-						outer_bp.getEIDFld(BPJoinNodePosition).getValue(), RightConfidenceFilter);
+						try{
+							rstream = new Stream(_rdfdb, RightSubjectFilter, RightPredicateFilter, 
+							outer_bp.getEIDFld(BPJoinNodePosition).getValue(), RightConfidenceFilter);
+						}
+						catch(Exception e){
+							try{
+								rstream.closestream();
+								rstream = null;
+								continue;
+							}
+							catch(Exception ex){
+								rstream = null;
+								continue;
+							}
+						}
 					}
+				}
 				}
 			}
 		
